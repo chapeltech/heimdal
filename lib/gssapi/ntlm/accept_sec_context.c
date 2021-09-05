73e90e6c8dde4c8b981482e719668940b47b1407
Roland C. Dowdeswell <elric@imrryr.org>
2021-09-05 21:44:44 +0100

    fix memory leak near NTLM type2 response

--- lib/gssapi/ntlm/accept_sec_context.c
+++ lib/gssapi/ntlm/accept_sec_context.c
@@ -177,6 +177,7 @@ _gss_ntlm_accept_sec_context
 	}
 	memcpy(output_token->value, out.data, out.length);
 	output_token->length = out.length;
+	heim_ntlm_free_buf(&out);
 
 	ctx->flags = retflags;
 
